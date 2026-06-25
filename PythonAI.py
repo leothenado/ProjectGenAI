cf4fcc04606d4c18a94ede2f411e2397366c4ae9
leothenado <ajiteshsethi25@gmail.com>
2026-06-25 16:50:37 +0530

    Create PythonAI.py
    First Try

---
+++ PythonAI.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from google import genai
+st.markdown(
+    """
+    <h1 style='text-align: center;'>Python AI Assistant</h1>
+    <p style='text-align: center; font-size:18px;'>
+        Ask any python programming question.
+    </p>
+    """,
+    unsafe_allow_html=True,
+)
+jarvis = genai.Client(api_key="MY_API")
+mychat = jarvis.chats.create(model = "gemini-flash-lite-latest") #to hold the history of conversation
+#Placeholder for the response
+response_placeholder = st.empty()
+question = st.text_input(" ", placeholder="Enter your python question here...")
+
+col1, col2, col3 = st.columns([4, 1, 4])
+
+with col2:
+    send = st.button ("Send")
+
+if send:
+    response = mychat.send_message(question)
+    st.write(response.text)
