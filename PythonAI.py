5ab69ac52a25dcde1ea5dd01641cc0636380c778
leothenado <ajiteshsethi25@gmail.com>
2026-06-26 14:54:30 +0530

    Update PythonAI.py

--- PythonAI.py
+++ PythonAI.py
@@ -2,9 +2,9 @@ import streamlit as st
 from google import genai
 st.markdown(
     """
-    <h1 style='text-align: center;'>Python AI Assistant</h1>
+    <h1 style='text-align: center;'>AI Assistant</h1>
     <p style='text-align: center; font-size:18px;'>
-        Ask any python programming question.
+        Ask any question. 1Shoe is here for you!
     </p>
     """,
     unsafe_allow_html=True,
@@ -13,7 +13,7 @@ jarvis = genai.Client(api_key=st.secrets["MY_API"])
 mychat = jarvis.chats.create(model = "gemini-flash-lite-latest") #to hold the history of conversation
 #Placeholder for the response
 response_placeholder = st.empty()
-question = st.text_input(" ", placeholder="Enter your python question here...")
+question = st.text_input(" ", placeholder="Enter your question here...")
 
 col1, col2, col3 = st.columns([4, 1, 4])
 
